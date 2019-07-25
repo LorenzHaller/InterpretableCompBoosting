62fec52dcab10c0ad44765515a5c140c05d5fd55
Lorenz <lorenz_haller@web.de>
2019-07-25 18:21:32 +0200

    small changes

--- treephase.R
+++ treephase.R
@@ -1,2 +1,7 @@
-mboost_bs = mboost::mboost(formula = formula, data = data, baselearner = "bbs",
+# Start by comparing to mboost tree method
+
+mboost_btree = mboost::mboost(formula = formula, data = data, baselearner = "btree",
                            control = boost_control(mstop = 500))
+str(mboost_btree)
+mboost_btree$response
+mboost_btree$basemodel
