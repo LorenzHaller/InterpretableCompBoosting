0c2e7e034cb264f7190463c7667f995b472c5cf9
Lorenz <lorenz_haller@web.de>
2019-08-22 19:06:51 +0200

    Improve benchmark plots.

--- Benchmark_v1.R
+++ Benchmark_v1.R
@@ -1,6 +1,8 @@
-# Benchmark to compare test and train errors
+######################################################################################
+#### Benchmark to compare test and train errors: Airquality Data Example
+######################################################################################
 
-# Airquality Data Example
+##### PREPARATION ####################################################################
 
 setwd("C:/Users/halle/Downloads/Uni/Interpretable Machine Learning/InterpretableCompBoosting")
 
@@ -10,7 +12,7 @@ attach(airquality)
 data <- na.omit(airquality)
 
 # Split the data in training and test data (75/25 split)
-set.seed(1492)
+set.seed(1995)
 sample <- sample.int(n = nrow(data), size = floor(.66*nrow(data)), replace = F)
 train <- data[sample, ]
 test  <- data[-sample, ]
@@ -23,50 +25,68 @@ formula <- terms.formula(formula)
 source("family.R")
 
 
+### SET PARAMETERS ###################################################################
 
-##############################
+mstop_bm = 200
+nu_bm = 0.05
 
-# Applying combined method to data (MBOOST VERSION)
+
+##### MODEL TRAINING #################################################################
+
+## OWN METHOD ########################################################################
 source("linearmodels_splines_trees_mboost.R")
-micb_500 = interpretable_comp_boost_m(train, formula, nu=0.05, mstop=200, family=Gaussian(),
-                                      epsilon = 0.0025)
+micb_500 = interpretable_comp_boost_m(train, formula, nu=nu_bm, mstop=mstop_bm, 
+                                      family=Gaussian(),epsilon = 0.0025)
 avg_risk = micb_500$Risk / dim(train)[1]
-
+# Make predictions
 source("icb_predict.R")
 pred = icb_predict(icb_object = micb_500, newdata = test, target="Ozone")
 
 avg_risk_test = pred$TestRisk / dim(test)[1]
 
-
-
-# Plot the risk vs the number of iterations
-plot(1:length(micb_500$Risk),avg_risk, xlab="Iteration",ylab="Risk",col="blue",type="l", 
-     ylim=c(0,1000),xlim=c(0,micb_500$Input_Parameters[2]))
-points(1:length(avg_risk_test),avg_risk_test,type="l",col="darkgreen")
-
+## MBOOST METHODS #######################################################################
 
 library(mboost)
-
 # Using mboost with splines
 mboost_bols_bs = mboost::gamboost(formula = formula, data = train, baselearner = "bbs",
-                                  control = boost_control(nu = 0.05, mstop = 200))
-
+                                  control = boost_control(nu = nu_bm, mstop = mstop_bm))
+mb_spline_pred = mboost_bols_bs$predict(test)
 # Checking mboost with trees
 mboost_tree = mboost::mboost(formula = formula, data = train, baselearner = "btree",
-                             control = boost_control(nu = 0.05, mstop = 200))
-
-# Combine to mboost using splines
-points(1:length(mboost_bols_bs$risk()),mboost_bols_bs$risk()/dim(train)[1],type="l",col="red")
-# Combine to mboost using trees
-points(1:length(mboost_tree$risk()),mboost_tree$risk()/dim(train)[1],type="l",col="orange")
-
-mb_spline_pred = mboost_bols_bs$predict(test)
-h_pred=micb_500$Riskfunction(y=test$Ozone,f=mb_spline_pred)/dim(test)[1]
-abline(h=h_pred)
-
+                             control = boost_control(nu = nu_bm, mstop = mstop_bm))
 mb_tree_pred = mboost_tree$predict(test)
+
+
+##### VISUALISATION / COMPARISON #########################################################
+
+##### Plot the risk vs the number of iterations
+
+plot(1:length(micb_500$Risk),avg_risk, xlab="Iteration",ylab="Average Risk",col="red",type="l", 
+     ylim=c(0,1000),xlim=c(0,micb_500$Input_Parameters[2]))
+points(1:length(avg_risk_test),avg_risk_test,type="o",col="red")
+
+# Mboost using splines
+points(1:length(mboost_bols_bs$risk()),mboost_bols_bs$risk()/dim(train)[1],type="l",col="blue")
+h_pred=micb_500$Riskfunction(y=test$Ozone,f=mb_spline_pred)/dim(test)[1]
+abline(h=h_pred, col="blue")
+# Combine to mboost using trees
+points(1:length(mboost_tree$risk()),mboost_tree$risk()/dim(train)[1],type="l",col="green")
 h_pred_tree=micb_500$Riskfunction(y=test$Ozone,f=mb_tree_pred)/dim(test)[1]
-abline(h=h_pred_tree)
+abline(h=h_pred_tree, col = "green")
+
+# Add a legend to the plot
+legend(60,1000, 
+       legend=c("Own method", "Mboost using only own splines","Mboost using trees"),
+       col=c("red", "blue","green"), 
+       lty=1:2, 
+       cex=0.75)
+
+
+
+
+
+
+
 #################################
 
 ## Visualize the individual coefficients of the three phases
