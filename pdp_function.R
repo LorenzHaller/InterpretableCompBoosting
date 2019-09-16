e884bea3dd13997c7d7a9c4677100486f7629e29
Lorenz <lorenz_haller@web.de>
2019-09-16 20:38:16 +0200

    adjust/update partial dependence plot function

--- pdp_function.R
+++ pdp_function.R
@@ -2,8 +2,6 @@ pdp_function <- function(icb_object, newdata = NULL, ylim = NULL){
   
   data = icb_object$Data[,-1]
   feature_names = colnames(data)
-  feature_means = icb_object$`Feature Statistics`$Means[-1]
-  feature_stdevs = icb_object$`Feature Statistics`$StdDevs[-1]
   
   which = as.integer(seq(1,dim(data)[2],by=1))
   
@@ -15,38 +13,37 @@ pdp_function <- function(icb_object, newdata = NULL, ylim = NULL){
     data_temp = data[,w,drop=FALSE]
     feature_name = feature_names[w]
     
-    linear_coefficients = icb_object$Coefficients$Linear_coefficients[w+1]
-    rescaled_linear_coefficients = linear_coefficients / feature_stdevs[w]
-    spline_coefficients = icb_object$Coefficients[w+2]
-    #rescaled_intercept = Bs*Xmean/sdx
-    
+    linear_coefficients = icb_object$Prediction_Models$Linear$coef()[w]
+    intercept = linear_coefficients[[1]][[1]]
+    slope = linear_coefficients[[1]][[2]]
+    spline_coefficients = icb_object$Prediction_Models$Spline$coef()[w]
+    #mixed_coefficients = spline_coefficients + intercept + slope + 
     
     ## Extract the spline design matrix for the feature
-    design_matrix_found <- FALSE
     iteration <- icb_object$`Transition Iterations`[1]+1
-    while(design_matrix_found == FALSE){
-      object <- icb_object$Prediction_Models[[iteration]]
-      if(names(object$coef()) == paste("bbs(",feature_name,", df = dfbase)",sep="")){
-        basis <- extract(icb_object$Prediction_Models[iteration][[1]],"design")[[1]]
-        knots <- attr(basis,"knots")
-        design_matrix_found <- TRUE
+    
+    object <- icb_object$Prediction_Models$Spline
+    
+    
+    if(names(object$coef())[w] == paste("bbs(",feature_name,", df = dfbase)",sep="")){
+      basis <- extract(object,"design")[[w]]
+      knots <- attr(basis,"knots")
       } else{
-        iteration <- iteration + 1
-      }
+      iteration <- iteration + 1
     }
     
     
-    prod = basis %*% (spline_coefficients[[1]])
-                      #*feature_stdevs[w])
+    
+    prod = basis %*% spline_coefficients[[1]]
+                 
     
     pr = prod
     #pr = data_temp * rescaled_linear_coefficients + prod
     if (is.null(ylim)) ylim <- range(pr, na.rm = TRUE)
     
-    if(!is.factor(data_temp[[1]])){
+    if(!is.factor(data_temp[,1])){
       plot(sort(data_temp[,1]), pr[order(data_temp[,1]),1],type="b",
            ylab="",xlab=colnames(data_temp)[1], ylim = ylim)
-    
     }
     
   }
