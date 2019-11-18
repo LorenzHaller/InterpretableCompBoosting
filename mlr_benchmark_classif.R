2d186932a29f72fe9da31ffbbb67bfc8fc8b744c
Lorenz <lorenz_haller@web.de>
2019-11-18 15:25:28 +0100

    Start the classification benchmark

--- mlr_benchmark_classif.R
+++ mlr_benchmark_classif.R
@@ -9,17 +9,17 @@ source("mlr_wrapper.R")
 set.seed(177)
 
 # Multiple learners to be compared
-lrns.classif = list(makeLearner("classif.icb",par.vals = list(nu=0.1, epsilon = 0.005, bl2="btree", max_depth = 4)),
-            makeLearner("classif.binomial"),
-            #makeLearner("classif.gamboost"),
-            makeLearner("classif.glmboost"),
-            makeLearner("classif.rpart"),
-            makeLearner("classif.gbm"),
-            makeLearner("classif.ada"), 
-            makeLearner("classif.kknn"),
-            makeLearner("classif.svm"),
-            makeLearner("classif.randomForest")
-)
+# lrns.classif = list(makeLearner("classif.icb",par.vals = list(nu=0.1, epsilon = 0.005, bl2="btree", max_depth = 4)),
+#             makeLearner("classif.binomial"),
+#             #makeLearner("classif.gamboost"),
+#             makeLearner("classif.glmboost"),
+#             makeLearner("classif.rpart"),
+#             makeLearner("classif.gbm"),
+#             makeLearner("classif.ada"), 
+#             makeLearner("classif.kknn"),
+#             makeLearner("classif.svm"),
+#             makeLearner("classif.randomForest")
+# )
 
 
 # Choose the resampling strategy
@@ -27,40 +27,150 @@ rdesc = makeResampleDesc("CV",iters=5)
 
 # Make a task
 
-taskinfo_all_classif = listOMLTasks(task.type = "Supervised Classification", limit = 50,
-                            number.of.instances = c(1000,100000),
-                            number.of.features = c(5,150), number.of.classes = 2)
-taskinfo_all_classif
+taskinfo_all_classif = listOMLTasks(task.type = "Supervised Classification",
+                            number.of.instances = c(500,10000),
+                            number.of.features = c(5,30), number.of.classes = 2)
+taskinfo_all_classif = taskinfo_all_classif[!duplicated(taskinfo_all_classif$name) & taskinfo_all_classif$number.of.instances.with.missing.values == 0,]
 
-# Task 1: spam -> from mlr -> spam.task
-
-# Task 2: credit
-credit.OML.task = getOMLTask(31)
-credit = na.omit(credit.OML.task$input$data.set$data)
-credit.task = makeClassifTask(data = credit, target = "class")
-
-# Task 3: electricity
-electricity.OML.task = getOMLTask(219)
-electricity = na.omit(electricity.OML.task$input$data.set$data)
-electricity.task = makeClassifTask(data = electricity, target = "class")
-
-# Task 4: BNG id 322
-bng.OML.task = getOMLTask(206)
-bng = na.omit(bng.OML.task$input$data.set$data)
-bng.task = makeClassifTask(data = bng, target = "Class")
-
-# Task 5: adult (2071)
-mammo.OML.task = getOMLTask(3048)
-mammo = na.omit(mammo.OML.task$input$data.set$data)
-mammo.task = makeClassifTask(data = mammo, target = "class")
+taskinfo_all_classif_num = taskinfo_all_classif[taskinfo_all_classif$number.of.symbolic.features < 2,]
 
 
-# Make benchmark
-bmr.classif = benchmark(lrns.classif, mammo.task, rdesc_v2)
+## Task 1: churn
+churn.OML.task = getOMLTask(146227)
+churn = churn.OML.task$input$data.set$data
+churn.task = makeClassifTask(data = churn, target = "class")
+
+## Task 2: Bank-marketing
+bank.OML.task = getOMLTask(9899)
+bank = bank.OML.task$input$data.set$data
+bank.task = makeClassifTask(data = bank, target = "Class")
+
+## Task 3: dis
+dis.OML.task = getOMLTask(146239)
+dis = dis.OML.task$input$data.set$data
+dis.task = makeClassifTask(data = dis, target = "class")
+
+## Task 4: Japanese Vowels
+Jap.OML.task = getOMLTask(3839)
+Jap = Jap.OML.task$input$data.set$data
+Jap.task = makeClassifTask(data = Jap, target = "binaryClass")
+
+## Task 5: delta_elevators
+delta.OML.task = getOMLTask(3684)
+delta = delta.OML.task$input$data.set$data
+delta.task = makeClassifTask(data = delta, target = "binaryClass")
+
+## Task 6: pollen
+pollen.OML.task = getOMLTask(3775)
+pollen = pollen.OML.task$input$data.set$data
+pollen.task = makeClassifTask(data = pollen, target = "binaryClass")
 
 
-getBMRAggrPerformances(bmr.classif)
-plt = plotBMRBoxplots(bmr.classif,pretty.names = F)
-#levels(plt$data$learner.id) = c("LDA", "CART", "RF")
-plt + theme(axis.text.x = element_text(angle = -75))
-getBMRPerformances(bmr.classif, as.df = TRUE)
+
+####### Hyperparametertuning Part ##################################################
+
+tsk = churn.task
+
+#ctrl = makeTuneControlGrid()
+ctrl = makeTuneControlRandom(maxit = 30L)
+inner = makeResampleDesc("CV", iters = 2L)
+outer = makeResampleDesc("CV", iters = 5L)
+
+####### Hypertuning for icb method 
+
+# icb using tree stumps
+set.seed(177)
+num_ps_tree = makeParamSet(
+  makeNumericParam("nu", lower = 0.001, upper = 0.2),
+  makeNumericParam("epsilon", lower = 0.0005, upper = 0.1),
+  makeDiscreteLearnerParam(id = "bl2", default = "btree", values = c("btree"), tunable = F),
+  makeIntegerLearnerParam(id = "max_depth", lower = 3, upper = 8, tunable = T)
+)
+
+icb.tree = makeTuneWrapper("classif.icb", resampling = inner, par.set = num_ps_tree,
+                           control = ctrl)
+
+r = resample(icb.tree, tsk, resampling = outer, extract = getTuneResult)
+
+# icb using splines
+
+num_ps_spline = makeParamSet(
+  makeNumericParam("nu", lower = 0.001, upper = 0.2),
+  makeNumericParam("epsilon", lower = 0.0005, upper = 0.1),
+  makeDiscreteLearnerParam(id = "bl2", default = "bbs", values = c("bbs"), tunable = F),
+  makeIntegerLearnerParam(id = "max_depth", lower = 3, upper = 8, tunable = T),
+  makeIntegerLearnerParam(id = "df_spline", lower = 2L, upper = 5L, tunable = T)
+)
+
+icb.spline = makeTuneWrapper("regr.icb", resampling = inner, par.set = num_ps_spline,
+                             control = ctrl)
+
+r.icb.spline = resample(icb.spline, tsk, resampling = outer, extract = getTuneResult)
+
+
+# ksvm
+set.seed(177)
+ps_ksvm = makeParamSet(
+  makeNumericParam("C", lower = -15, upper = 15, trafo = function(x) 2^x),
+  makeNumericParam("sigma", lower = -15, upper = 15, trafo = function(x) 2^x)
+)
+
+ksvm = makeTuneWrapper("regr.ksvm", resampling = inner, par.set = ps_ksvm,
+                       control = ctrl, show.info = FALSE)
+
+r.ksvm = resample(ksvm, tsk, resampling = outer, extract = getTuneResult)
+
+
+
+# random Forest
+
+params.rf <- makeParamSet(makeIntegerParam("mtry",lower = 2,upper = 5),
+                          makeIntegerParam("nodesize",lower = 5,upper = 50),
+                          makeIntegerParam("ntree", lower = 100, upper = 1000))
+rf = makeTuneWrapper("regr.randomForest", resampling = inner, par.set = params.rf,
+                     control = ctrl, show.info = FALSE)
+r.rf = resample(rf, tsk, resampling = outer, extract = getTuneResult)
+
+
+# linear model
+lm = makeLearner("regr.lm")
+r.lm = resample(lm, tsk, resampling = outer)
+
+
+# gamboost
+params.gamboost <- makeParamSet(makeIntegerParam("mstop", lower = 50, upper = 1000),
+                                makeNumericParam("nu", lower = 0.01, upper = 0.2)
+)
+gamb = makeTuneWrapper("regr.gamboost", resampling = inner, par.set = params.gamboost,
+                       control = ctrl, show.info = FALSE)
+r.gamb = resample(gamb, tsk, resampling = outer, extract = getTuneResult)
+
+
+# glmboost
+params.glmboost <- makeParamSet(makeIntegerParam("mstop", lower = 50, upper = 1000),
+                                makeNumericParam("nu", lower = 0.01, upper = 0.2))
+glmb = makeTuneWrapper("regr.glmboost", resampling = inner, par.set = params.glmboost,
+                       control = ctrl, show.info = FALSE)
+r.glmb = resample(glmb, tsk, resampling = outer, extract = getTuneResult)
+
+
+# Tuning for an rpart tree
+params.rpart = makeParamSet(
+  makeIntegerParam("maxdepth", lower = 1, upper = 20),
+  makeNumericParam("cp", lower = 0, upper = 1)
+)
+rpart = makeTuneWrapper("regr.rpart", resampling = inner, par.set = params.rpart,
+                        control = ctrl, show.info = FALSE)
+r.rpart = resample(rpart, tsk, resampling = outer, extract = getTuneResult)
+
+
+# Tuning for xgboost
+params.xgboost = makeParamSet(
+  makeIntegerParam ("max_depth" , lower = 1, upper = 10),
+  makeIntegerParam("nrounds", lower = 1, upper = 1000)
+)
+xgb = makeTuneWrapper("regr.xgboost", resampling = inner, par.set = params.xgboost,
+                      control = ctrl, show.info = FALSE)
+r.xgb = resample(xgb, tsk, resampling = outer, extract = getTuneResult)
+
+
